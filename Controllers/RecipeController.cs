4d74beaf98ae5336eff98fedafb365cb1f2e7598
WAHIB UL MALIK <jayedw1568@gmail.com>
2023-12-29 23:48:50 +0300

    Add files via upload

---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeApplication.Data;
+using RecipeApplication.Models;
+using System.Collections.Generic;
+
+namespace RecipeApplication.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly Recipes recipes;
+
+        public RecipeController(Recipes recipes)
+        {
+            this.recipes = recipes;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            var recipes = new Recipes().List();
+            return View(recipes);
+        }
+
+        public ActionResult Details(Guid recipeId)
+        {
+            var recipe = new Recipes().FindById(recipeId);
+            return View(recipe);
+        }
+
+        public ActionResult Add()
+        {
+            var newRecipe = new Recipe() { ID = Guid.NewGuid() };
+            return View(newRecipe);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Recipe newRecipe)
+        {
+            if (ModelState.IsValid)
+            {
+                recipes.Add(newRecipe);
+                return RedirectToAction(nameof(List));
+            }
+            return View(newRecipe);
+        }
+
+        public ActionResult Edit(Guid recipeId)
+        {
+
+            var recipe = new Recipes().FindById(recipeId);
+            return View(recipe);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Recipe editedRecipe)
+        {
+            if (ModelState.IsValid)
+            {
+                recipes.Update(editedRecipe);
+                return RedirectToAction(nameof(List));
+            }
+            return View(editedRecipe);
+        }
+
+        public ActionResult Delete(Guid recipeId)
+        {
+            recipes.Remove(recipeId);
+            return RedirectToAction(nameof(List));
+        }
+    }
+}
---
+++ Data/Recipes.cs
@@ -0,0 +1,54 @@
+﻿using RecipeApplication.Models;
+
+namespace RecipeApplication.Data
+{
+    public class Recipes
+    {
+    readonly List<Recipe> recipes = new(){
+        new()
+        {
+            Name = "Sweet Halva",
+            Ingredients = @"Wheat flour 2 cups
+            Sugar 1 cup
+            Butter 1/2 cups
+            Water 1 cup
+            Rosewater 4 tablespoons
+            Saffron 1/4 tsp
+            Cardamom powder 1/4 tsp",
+            Instructions = @"Keep a pan on a moderate flame and add 2 cups of flour to it.
+            Stir and sauté the flour and be careful not to burn it.
+            Your flour should be golden in color in order for you to move to the next step.
+            Now it’s time for you to add 1/2 cup of butter or oil to your flour.
+            Pour 1/4 teaspoon of ground saffron on 2 ice cubes. Let the ice cubes melt. Our bloomed saffron is ready.
+            Pour a cup of water into a small pan on heat.
+            Then add 1/4 teaspoon cardamom powder, 1 cup of sugar, 4 tablespoons of rosewater, and bloomed saffron to the water. Mix them well and make sure you do not over-add rosewater or cardamon. Halva syrup is ready!
+            Add syrup to the flour and mix them on low heat again. Stir it frequently for some minutes. Be careful do not burn it!
+            Your halva is ready. Take a plate and spread it on the plate. You can add designs to your halva and decorate it with almonds, coconut, or even pistachios."
+        },
+
+        
+    };
+
+        public IEnumerable<Recipe> List()
+        => recipes;
+
+        public Recipe FindById(Guid recipeId)
+        => recipes.Single(recipe => recipe.ID == recipeId);
+
+        public void Add(Recipe recipe)
+        => recipes.Add(recipe);
+
+        public void Update(Recipe newRecipe)
+        {
+            var oldRecipe = recipes.Single(r => r.ID == newRecipe.ID);
+
+            oldRecipe.Name = newRecipe.Name;
+            oldRecipe.Ingredients = newRecipe.Ingredients;
+            oldRecipe.Instructions = newRecipe.Instructions;
+        }
+
+        public void Remove(Guid recipeId)
+        => recipes.Remove(FindById(recipeId));
+    }
+
+}
